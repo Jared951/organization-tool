232a16857b73b3028c349dd0b657ebee711858f2
Jared Sewak <jaredsewak@gmail.com>
2023-08-23 03:55:49 -0600

    Fixed login and registration, password hashing, and database

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
 
     __tablename__ = "users"
 
@@ -18,8 +19,14 @@ class User(db.Model):
         self.email = email
         self.password_hash = generate_password_hash(password)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password) 
+    
+    def is_active(self):
+        return True
 
 class Task(db.Model):
 
--- server.py
+++ server.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
-from model import User, Task, db, connect_to_db
-from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, flash, current_app
+from flask_login import LoginManager, login_required, login_user, logout_user
+from model import db, connect_to_db, User
 from forms import RegistrationForm, LoginForm
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 connect_to_db(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -21,20 +22,17 @@ def register():
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
-        hashed_password = generate_password_hash(password, method='sha256')
         
-        new_user = User(email=email, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()  
+        new_user = User(email=email, password=password)  # Pass both email and password
+        new_user.set_password(password)  # Set the hashed password using set_password method
+        
+        with current_app.app_context():
+            db.session.add(new_user)
+            db.session.commit()
 
         flash('Account created successfully! You can now log in.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -43,13 +41,19 @@ def login():
         email = form.email.data
         password = form.password.data
         user = User.query.filter_by(email=email).first()
-        if user and user.check_password(password):
-            login_user(user)
+        
+        if user and user.check_password(password):  # Use check_password method
             flash('Logged in successfully!', 'success')
+            login_user(user)
             return redirect(url_for('home'))
         else:
             flash('Login failed. Please check your credentials.', 'danger')
+    
     return render_template('login.html', form=form)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/logout')
 @login_required
@@ -61,66 +65,7 @@ def logout():
 @app.route('/')
 @login_required
 def home():
-    user = current_user
-    tasks = user.tasks
-    return render_template('home.html', tasks=tasks)
-
-@app.route('/create_event', methods=['GET', 'POST'])
-@login_required
-def create_event():
-    if request.method == 'POST':
-        # Get data from the form
-        name = request.form['name']
-        company = request.form['company']
-        due_date = datetime.strptime(request.form['due_date'], '%Y-%m-%d')
-        due_time = datetime.strptime(request.form['due_time'], '%H:%M').time()
-        notes = request.form['notes']
-
-        # Validate form data (add your validation logic here)
-        if not name or not company or not due_date or not due_time:
-            flash('Please fill in all required fields.', 'danger')
-            return redirect(url_for('create_event'))
-
-        try:
-            # Create a new Task object and add it to the database
-            user_id = current_user.id  # Use the logged-in user's ID
-            new_task = Task(name=name, company=company, due_date=due_date, due_time=due_time, notes=notes, user_id=user_id)
-            db.session.add(new_task)
-            db.session.commit()
-            flash('New task created successfully!', 'success')
-            return redirect(url_for('home'))
-        except Exception as e:
-            db.session.rollback()
-            flash('An error occurred while creating the task. Please try again later.', 'danger')
-            print(f"Error: {e}")
-
-    return render_template('create_event.html')
-
-
-@app.route('/edit_event/<int:event_id>', methods=['GET', 'POST'])
-@login_required
-def edit_event(event_id):
-    task = Task.query.get(event_id)
-    if request.method == 'POST':
-        # Update event data based on form input
-        task.name = request.form['name']
-        task.company = request.form['company']
-        task.due_date = datetime.strptime(request.form['due_date'], '%Y-%m-%d')
-        task.due_time = datetime.strptime(request.form['due_time'], '%H:%M').time()
-        task.notes = request.form['notes']
-        db.session.commit()
-
-        return redirect(url_for('home'))
-
-    return render_template('edit_event.html', task=task)
-
-@app.route('/delete_event/<int:event_id>', methods=['POST'])
-@login_required
-def delete_event(event_id):
-    task = Task.query.get(event_id)
-    db.session.delete(task)
-    db.session.commit()
-    return redirect(url_for('home'))
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
